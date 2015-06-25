0645f57c16c3ff2b85b39f62ea86e7d503eabefe
Fabrício da Cunha Santos <fabriciodacunhasantos@gmail.com>
2015-06-24 22:27:11 -0300

    Inicialização do Projeto

---
+++ src/main/java/br/com/gl/futebol/GlFutebolApplication.java
@@ -0,0 +1,12 @@
+package br.com.gl.futebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlFutebolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlFutebolApplication.class, args);
+    }
+}
