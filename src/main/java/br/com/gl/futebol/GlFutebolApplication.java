29de535d06a7d3afda63184413d1dcf7a4b0100c
Fabrício da Cunha Santos <fabriciodacunhasantos@gmail.com>
2015-06-24 23:31:51 -0300

    Instalação do JSF

--- src/main/java/br/com/gl/futebol/GlFutebolApplication.java
+++ src/main/java/br/com/gl/futebol/GlFutebolApplication.java
@@ -1,12 +1,32 @@
 package br.com.gl.futebol;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.faces.webapp.FacesServlet;
 
-@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class GlFutebolApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GlFutebolApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GlFutebolApplication.class, args);
+	}
+
+	@Bean
+	public FacesServlet facesServlet() {
+		return new FacesServlet();
+	}
+
+	@Bean
+	public ServletRegistrationBean facesServletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(
+				facesServlet(), "*.xhtml");
+		return registration;
+	}
 }
